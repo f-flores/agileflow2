47cf6977f39ea65110c2fbb41c88c3b5ae1eeed9
Fabian Flores <ffflores1@outlook.com>
2018-10-01 20:34:24 -0400

    isAuthenticated.js

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,5 +1,3 @@
-// boilerplate code from Rutgers Coding bootcamp
-
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
 module.exports = function(req, res, next) {
   // If the user is logged in, continue with the request to the restricted route
